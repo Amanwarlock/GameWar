8edc37f05149a0a418de306bd2f9b6714f3a3e20
amanwarlock <amanwarlock@rocketmail.com>
2021-11-05 16:07:40 -0700

    Implementation of game's core functionality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+from classes.deck import Deck
+from classes.player import Player
+from helpers.notifier import Notifier
 """
     Main class - Entry point that is responsible for creating players, deck of cards, shuffling and
     distributing the cards among players
@@ -30,19 +32,124 @@ class Main:
 
     # Entry point that kick starts the game
     def start(self):
-        pass
+        Notifier.fire('\n \t\t\t\tLet\'s Play WAR of Cards !')
+        player_one_name = input('\nEnter Player-1 name: ')
+        player_two_name = input('\nEnter Player-2 name: ')
+        self.create_players(player_one_name, player_two_name)
+        self.initialize_deck()
+        self.start_game()
+        Notifier.notify_game_stats(self.deck.round, self.total_wars, self.winner)
 
     # Function to create two player using the given player names
     def create_players(self, player_one_name, player_two_name):
-        pass
+        self.player_one = Player(player_one_name)
+        self.player_two = Player(player_two_name)
+        Notifier.fire(f'\n\n Players Created! \n\n Welcome {self.player_one.get_player_name()} and '
+                      f'{self.player_two.get_player_name()} onboard !\n')
 
     # Function creates instances of cards (52) and adds to the deck array - uses a builder design pattern
     def initialize_deck(self):
-        pass
+        self.deck = Deck()
+        self.deck.build_deck().distribute(self.player_one, self.player_two)
 
     # Function starts the gameplay once deck and players are initialized
     def start_game(self):
-        pass
+        self.is_game_on = True
+
+        while self.is_game_on:
+            self.deck.inc_round()
+            Notifier.fire(f'------------------ ROUND - {self.deck.round} -----------------------')
+
+            if self.player_one.player_cards_count() == 0 and self.player_two.player_cards_count() > 0:
+                Notifier.notify_game_winner(self.TITLE_PLAYER_TWO, self.player_two)
+                self.winner = self.player_two
+                self.is_game_on = False
+                break
+
+            if self.player_two.player_cards_count() == 0 and self.player_one.player_cards_count() > 0:
+                Notifier.notify_game_winner(self.TITLE_PLAYER_ONE, self.player_one)
+                self.winner = self.player_one
+                self.is_game_on = False
+                break
+
+            if self.player_one.player_cards_count() == 0 and self.player_two.player_cards_count() == 0:
+                Notifier.notify_match_draw()
+                self.is_draw = True
+                self.is_game_on = False
+                break
+
+            player_one_card = self.player_one.draw_one()
+            player_two_card = self.player_two.draw_one()
+
+            Notifier.fire(f'\n')
+            Notifier.notify_card_drawn(self.TITLE_PLAYER_ONE, self.player_one, player_one_card)
+            Notifier.notify_card_drawn(self.TITLE_PLAYER_TWO, self.player_two, player_two_card)
+
+            result = player_one_card.compare(player_two_card)
+
+            if result == 1:
+                self.player_one.add_cards([player_one_card, player_two_card])
+                Notifier.notify_round_winner(self.TITLE_PLAYER_ONE, self.deck.round, self.player_one, self.player_two)
+            elif result == -1:
+                self.player_two.add_cards([player_two_card, player_one_card])
+                Notifier.notify_round_winner(self.TITLE_PLAYER_TWO, self.deck.round, self.player_two, self.player_one)
+            else:
+                Notifier.declare_war()
+                self.is_war = True
+
+                while self.is_war:
+
+                    self.track_wars()
+
+                    if self.player_one.player_cards_count() < 4:
+                        Notifier.notify_game_winner(self.TITLE_PLAYER_TWO, self.player_two)
+                        self.winner = self.player_two
+                        self.is_war = False
+                        self.is_game_on = False
+                        break
+
+                    if self.player_two.player_cards_count() < 4:
+                        Notifier.notify_game_winner(self.TITLE_PLAYER_ONE, self.player_one)
+                        self.winner = self.player_one
+                        self.is_war = False
+                        self.is_game_on = False
+                        break
+
+                    player_one_card_pool = [player_one_card]
+                    player_two_card_pool = [player_two_card]
+                    player_one_card_pool.extend(self.player_one.draw_many(3))
+                    Notifier.fire(f'{self.player_one.get_player_name()} drew three cards face-down')
+                    player_two_card_pool.extend(self.player_two.draw_many(3))
+                    Notifier.fire(f'{self.player_two.get_player_name()} drew three cards face-down')
+
+                    player_one_card = self.player_one.draw_one()
+                    Notifier.fire(
+                        f'Player-1: {self.player_one.get_player_name()} drew one card face-up {player_one_card.show()}')
+                    player_two_card = self.player_two.draw_one()
+                    Notifier.fire(
+                        f'Player-2: {self.player_two.get_player_name()} drew one card face-up {player_two_card.show()}')
+
+                    result = player_one_card.compare(player_two_card)
+
+                    if result == 1:
+                        player_one_card_pool.append(player_one_card)
+                        player_two_card_pool.append(player_two_card)
+                        player_one_card_pool.extend(player_two_card_pool)
+                        self.player_one.add_cards(player_one_card_pool)
+                        self.is_war = False
+                        Notifier.notify_round_winner(self.TITLE_PLAYER_ONE, self.deck.round, self.player_one,
+                                                     self.player_two)
+                    elif result == -1:
+                        player_one_card_pool.append(player_one_card)
+                        player_two_card_pool.append(player_two_card)
+                        player_two_card_pool.extend(player_one_card_pool)
+                        self.player_two.add_cards(player_two_card_pool)
+                        self.is_war = False
+                        Notifier.notify_round_winner(self.TITLE_PLAYER_TWO, self.deck.round, self.player_two,
+                                                     self.player_one)
+                    else:
+                        Notifier.declare_war()
+                        self.is_war = True
 
 
 if __name__ == '__main__':
--- test/main_test.py
+++ test/main_test.py
@@ -1,4 +1,9 @@
 import unittest
+from main import Main
+from classes.deck import Deck
+from classes.card import Card
+from enums.suits_enum import SuitEnum
+from enums.card_enum import CardEnum
 """
 Test file to validate the game play functionality
 """
@@ -8,31 +13,100 @@ class MainTest(unittest.TestCase):
 
     # Test scenario when outcome of the game is a draw i.e., when both the players simultaneously run out of cards
     def test_for_game_draw(self):
-        pass
+        game = Main()
+        game.create_players('Mike', 'Blake')
+        game.deck = Deck()
+        game.start_game()
+        self.assertEqual(game.is_draw, True)
+        self.assertEqual(game.is_game_on, False)
 
     # When the first player has no cards, the second player should win
     def test_when_player_one_has_zero_cards(self):
-        pass
+        game = Main()
+        game.create_players('Mike', 'Blake')
+        game.deck = Deck()
+        card_two = Card(SuitEnum.SPADES, CardEnum.TWO)
+        game.player_two.add_cards([card_two])
+        self.assertEqual(game.player_one.player_cards_count(), 0)
+        self.assertEqual(game.player_two.player_cards_count(), 1)
+        game.start_game()
+        self.assertEqual(game.winner.get_player_name(), 'Blake')
 
     # When the second player has no cards, the first should win
     def test_when_player_two_has_zero_cards(self):
-        pass
+        game = Main()
+        game.create_players('Mike', 'Blake')
+        game.deck = Deck()
+        card_two = Card(SuitEnum.SPADES, CardEnum.TWO)
+        game.player_one.add_cards([card_two])
+        self.assertEqual(game.player_two.player_cards_count(), 0)
+        self.assertEqual(game.player_one.player_cards_count(), 1)
+        game.start_game()
+        self.assertEqual(game.winner.get_player_name(), 'Mike')
 
     # When player one has obtained all the cards, it should be the winner
     def test_with_player_one_as_winner(self):
-        pass
+        game = Main()
+        game.create_players('Mike', 'Blake')
+        game.deck = Deck()
+        card_ace = Card(SuitEnum.SPADES, CardEnum.ACE)
+        card_two = Card(SuitEnum.SPADES, CardEnum.TWO)
+        game.player_one.add_cards([card_ace])
+        game.player_two.add_cards([card_two])
+        self.assertEqual(game.player_two.player_cards_count(), 1)
+        self.assertEqual(game.player_one.player_cards_count(), 1)
+        game.start_game()
+        self.assertEqual(game.winner.get_player_name(), 'Mike')
 
     # When player two acquired all the cards, it should be the sole winner
     def test_with_player_two_as_winner(self):
-        pass
+        game = Main()
+        game.create_players('Mike', 'Blake')
+        game.deck = Deck()
+        card_ace = Card(SuitEnum.SPADES, CardEnum.ACE)
+        card_two = Card(SuitEnum.SPADES, CardEnum.TWO)
+        game.player_one.add_cards([card_two])
+        game.player_two.add_cards([card_ace])
+        self.assertEqual(game.player_two.player_cards_count(), 1)
+        self.assertEqual(game.player_one.player_cards_count(), 1)
+        game.start_game()
+        self.assertEqual(game.winner.get_player_name(), 'Blake')
 
     # When both players draw the same card, battle of cards should begin
     def test_for_war(self):
-        pass
+        game = Main()
+        game.create_players('Mike', 'Blake')
+        game.deck = Deck()
+        card_two = Card(SuitEnum.SPADES, CardEnum.TWO)
+        card_four = Card(SuitEnum.SPADES, CardEnum.FOUR)
+        card_ten = Card(SuitEnum.SPADES, CardEnum.TEN)
+        card_ace = Card(SuitEnum.SPADES, CardEnum.ACE)
+        card_jack = Card(SuitEnum.SPADES, CardEnum.JACK)
+        game.player_one.add_cards([card_two, card_ace, card_four, card_jack, card_ten])
+        game.player_two.add_cards([card_two, card_four, card_ten, card_ace, card_jack])
+        self.assertEqual(game.player_two.player_cards_count(), 5)
+        self.assertEqual(game.player_one.player_cards_count(), 5)
+        game.start_game()
+        self.assertEqual(game.winner.get_player_name(), 'Blake')
+        self.assertEqual(game.winner.player_cards_count(), 10)
+        self.assertEqual(game.player_one.player_cards_count(), 0)
 
     # Edge case, during war, the player who runs out of cards first should lose
     def test_for_war_with_player_one_having_five_cards(self):
-        pass
+        game = Main()
+        game.create_players('Mike', 'Blake')
+        game.deck = Deck()
+        card_two = Card(SuitEnum.SPADES, CardEnum.TWO)
+        card_four = Card(SuitEnum.SPADES, CardEnum.FOUR)
+        card_ten = Card(SuitEnum.SPADES, CardEnum.TEN)
+        card_ace = Card(SuitEnum.SPADES, CardEnum.ACE)
+        card_jack = Card(SuitEnum.SPADES, CardEnum.JACK)
+        game.player_one.add_cards([card_two, card_ace, card_four, card_jack, card_ten])
+        game.player_two.add_cards([card_two, card_four, card_ten])
+        self.assertEqual(game.player_one.player_cards_count(), 5)
+        self.assertEqual(game.player_two.player_cards_count(), 3)
+        game.start_game()
+        self.assertEqual(game.winner.get_player_name(), 'Mike')
 
 
 if __name__ == '__main__':
